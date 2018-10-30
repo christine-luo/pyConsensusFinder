e7b4b6a01b1a9ef008506896c514915af6c7aa0e
christine-luo <41913854+christine-luo@users.noreply.github.com>
2018-10-30 12:14:42 -0500

    works now

--- modules/comparemutations.py
+++ modules/comparemutations.py
@@ -1,5 +1,4 @@
 import csv
-import numpy as np
 import os, sys
 
 thisdir = os.path.dirname(__file__)
@@ -10,12 +9,25 @@ if libdir not in sys.path:
 
 HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 MACHLEARN_MUTATIONS=[]
-with open(HOME+'/completed/TRAINING_MUTATIONS.csv', 'rb') as csv1:
-    with open(HOME+'/completed/MACHLEARN_MUTATIONS_ARRAY.csv') as csv2:
-            training_set = csv.reader(csv1, delimiter=',')
-            machlearn_set = csv.reader(csv2, delimiter=',')
-            for train_row in training_set:
-                for mach_row in machlearn_set:
-                    if mach_row[6].strip()==train_row[1].strip() and mach_row[7].strip()==train_row[2].strip() and mach_row[8].strip()==train_row[3].strip():
-                        MACHLEARN_MUTATIONS.append(mach_row)
-print MACHLEARN_MUTATIONS
+
+f1 = file(HOME+'/completed/MACHLEARN_MUTATIONS_ARRAY.csv', 'r')
+f2 = file(HOME+'/completed/TRAINING_MUTATIONS.csv', 'r')
+f3 = file(HOME+'/completed/machine_learning_mutations.csv', 'w')
+
+c1 = csv.reader(f1)
+c2 = csv.reader(f2)
+c3 = csv.writer(f3)
+
+traininglist = list(c2)
+
+for mach_row in c1:
+    row = 1
+    for training_row in traininglist:
+        results_row = mach_row
+        if mach_row[6].strip() == training_row[1].strip() and str(mach_row[7].strip()) == str(training_row[2].strip()) and mach_row[8].strip()==training_row[3].strip():
+            c3.writerow(results_row)
+        row = row + 1
+
+f1.close()
+f2.close()
+f3.close()
