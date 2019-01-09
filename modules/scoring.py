1e63152a1c09e313b184fe6d86af6d28a3b292fd
christine-luo <41913854+christine-luo@users.noreply.github.com>
2019-01-08 22:22:30 -0600

    Update scoring.py
    update how it's scored

--- modules/scoring.py
+++ modules/scoring.py
@@ -1,5 +1,6 @@
 import csv
 import os, sys
+import numpy as np
 
 thisdir = os.path.dirname(__file__)
 libdir = os.path.join(thisdir, '../modules')
@@ -9,22 +10,21 @@ if libdir not in sys.path:
 
 HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 
-f1 = file(HOME+'/completed/1EY0_trimmed_mutations.csv', 'r')
-f2 = file(HOME+'/completed/1EY0_scores.csv', 'w')
+f1 = file(HOME+'/completed/1vqb_trimmed_mutations.csv', 'r')
+f2 = file(HOME+'/completed/1vqb_scores.csv', 'w')
 
 c1 = csv.reader(f1)
 c2 = csv.writer(f2)
 
 prev_row = next(c1)
-print prev_row
 score = 0
 
 for current_row in c1:
     if current_row[0:5]==prev_row[0:5]:
-        if current_row[9]<0:
-            score = score-1
-        elif current_row[9]>0:
-            score = score+1
+        if float(current_row[9])<0:
+            score = score-(2*float(current_row[9])**2)
+        elif float(current_row[9]>0):
+            score = score+float(current_row[9])**2
     else:
         prev_row.append(score)
         index=[0,1,2,3,4,5,10]
