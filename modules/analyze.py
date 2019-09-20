186740d7ae652da967fd74c0390f5f966e0c0fa0
christine-luo <41913854+christine-luo@users.noreply.github.com>
2019-09-19 20:52:29 -0500

    Update analyze.py

--- modules/analyze.py
+++ modules/analyze.py
@@ -4,6 +4,10 @@ import numpy as np # need numpy for arrays and the like
 import Bio.SeqIO
 import Bio.Seq
 import CF
+import _mypath
+import os
+import math
+HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
  
 #Array of single letter amino acid cods for use in arrays. 
 IDS = np.zeros([22,1],dtype=object)
@@ -55,31 +59,31 @@ def trimmer(AAA, sequenceids=None, filename=None):
 def aacounts(AAA, filename=None): 
     COUNTS = np.zeros([22, len(AAA[0,:])],dtype=object) #makes array the length of the alingment with 22 rows (20AAs + "-" + "other")
     for index in range(len(AAA[0,:])): # for each position along the alingment, count occourances of each AA
-        COUNTS[0,index]=AAA[:,index].tolist().count("G")
-        COUNTS[1,index]=AAA[:,index].tolist().count("P")
-        COUNTS[2,index]=AAA[:,index].tolist().count("A")
-        COUNTS[3,index]=AAA[:,index].tolist().count("V")
-        COUNTS[4,index]=AAA[:,index].tolist().count("L")
-        COUNTS[5,index]=AAA[:,index].tolist().count("I")
-        COUNTS[6,index]=AAA[:,index].tolist().count("M")
-        COUNTS[7,index]=AAA[:,index].tolist().count("C")
-        COUNTS[8,index]=AAA[:,index].tolist().count("F")
-        COUNTS[9,index]=AAA[:,index].tolist().count("Y")
-        COUNTS[10,index]=AAA[:,index].tolist().count("W")
-        COUNTS[11,index]=AAA[:,index].tolist().count("H")
-        COUNTS[12,index]=AAA[:,index].tolist().count("K")
-        COUNTS[13,index]=AAA[:,index].tolist().count("R")
-        COUNTS[14,index]=AAA[:,index].tolist().count("Q")
-        COUNTS[15,index]=AAA[:,index].tolist().count("N")
-        COUNTS[16,index]=AAA[:,index].tolist().count("E")
-        COUNTS[17,index]=AAA[:,index].tolist().count("D")
-        COUNTS[18,index]=AAA[:,index].tolist().count("S")
-        COUNTS[19,index]=AAA[:,index].tolist().count("T")
-        COUNTS[20,index]=AAA[:,index].tolist().count("-") #empty spaces
+        print (AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[0,index]=-math.log(AAA[:,index].tolist().count("G")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[1,index]=-math.log(AAA[:,index].tolist().count("P")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[2,index]=-math.log(AAA[:,index].tolist().count("A")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[3,index]=-math.log(AAA[:,index].tolist().count("V")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[4,index]=-math.log(AAA[:,index].tolist().count("L")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[5,index]=-math.log(AAA[:,index].tolist().count("I")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[6,index]=-math.log(AAA[:,index].tolist().count("M")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[7,index]=-math.log(AAA[:,index].tolist().count("C")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[8,index]=-math.log(AAA[:,index].tolist().count("F")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[9,index]=-math.log(AAA[:,index].tolist().count("Y")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[10,index]=-math.log(AAA[:,index].tolist().count("W")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[11,index]=-math.log(AAA[:,index].tolist().count("H")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[12,index]=-math.log(AAA[:,index].tolist().count("K")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[13,index]=-math.log(AAA[:,index].tolist().count("R")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[14,index]=-math.log(AAA[:,index].tolist().count("Q")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[15,index]=-math.log(AAA[:,index].tolist().count("N")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[16,index]=-math.log(AAA[:,index].tolist().count("E")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[17,index]=-math.log(AAA[:,index].tolist().count("D")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[18,index]=-math.log(AAA[:,index].tolist().count("S")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[19,index]=-math.log(AAA[:,index].tolist().count("T")/AAA[:,index].tolist().count(AAA[0,index]))
+        COUNTS[20,index]=-math.log(AAA[:,index].tolist().count("-")/AAA[:,index].tolist().count(AAA[0,index]))#empty spaces
         COUNTS[21,index]=(len(AAA[:,index]) - sum(COUNTS[:,index].tolist())) #other, not counted above
     IDCOUNTS = np.hstack((IDS,COUNTS)) #make list with AA counts and names of AAs
-    if filename:
-        np.savetxt((filename),IDCOUNTS,delimiter=",",fmt="%s") #save file with AA names and counts
+    np.savetxt(("FREQTABLE.csv"),IDCOUNTS,delimiter=",",fmt="%s") #save file with AA names and counts
     return COUNTS
 
 #Returns a frequency array from an array of amino acid counts.
@@ -112,12 +116,12 @@ def aafrequencies(COUNTS, filename=None):
         FREQS[19,index]=np.float64(COUNTS[19,index])/sum(np.float64(COUNTS[:20,index]))
         FREQS[20,index]=np.float64(COUNTS[20,index])/sum(np.float64(COUNTS[:,index])) #frequency of gaps "-" as fraction of all seqs
         FREQS[21,index]=np.float64(COUNTS[21,index])/sum(np.float64(COUNTS[:,index])) #frequency of gaps "other" as fraction of all seqs
- 
+    #list of arrays, each position with a di
     #IDS=aaletters()
-    IDFREQS = np.hstack((IDS,FREQS)) #make list with names and AA frequencies
-    if filename:
-        np.savetxt((filename),IDFREQS,delimiter=",",fmt="%s") #save file with AA names and frequencies
-    return FREQS
+    IDFREQS = np.hstack((IDS,FREQS))  #make list with names and AA frequencies
+    CF.MACHLEARN_MUTATIONS.append(IDFREQS)
+    np.savetxt(("freqtable.csv"),IDFREQS,delimiter=",",fmt="%s") #save file with AA names and frequencies
+    return IDFREQS
 
 #Calculates the consensus sequence from given amino acid frequency array.
 #Returns consensus sequence as an array of amino acid one letter codes.
@@ -138,41 +142,39 @@ def consensus(FREQS, filename=None):
 #frequency array, and ratio. Will suggest mutations to consensus when query amino 
 #acids that differ from the consensus (i.e. highest frequency) by at least the ratio.
 #If given a filename, suggested mutations will be saved as txt file.
-def ratioconsensus(query, FREQS, ratio):    
-    mutations=[]
-    aalist = IDS.flatten().tolist()
-    for index in range(len(FREQS[0,:])): #for each AA position
-        wtaa = query[index]
-        consensus = IDS[np.argmax(FREQS[:20,index]),0]
-        if wtaa != consensus: #check if the consens residue is different than the query sequence
-            wtfreq = float(FREQS[(aalist.index(wtaa)),index])
-            consensusfreq = float(FREQS[(aalist.index(consensus)),index])
-            if (ratio * wtfreq) < consensusfreq: #if the consensus of a residue is greater than the threshold
-                #print "Residue number " + str(int(index) + 1)
-                #print str(int(100*consensusfreq)) + '% is at least ' + str(ratio) + ' times greater than ' + str(int(100*wtfreq)) + '%'
-                thissuggestion=res_to_change(wtaa,(index+1), consensus, consensusfreq, wtfreq)
-                mutations.append(thissuggestion)
-    return mutations
+# def ratioconsensus(query, FREQS, ratio):    
+#     mutations=[]
+#     aalist = IDS.flatten().tolist()
+#     for index in range(len(FREQS[0,:])): #for each AA position
+#         wtaa = query[index]
+#         consensus = IDS[np.argmax(FREQS[:20,index]),0]
+#         if wtaa != consensus: #check if the consens residue is different than the query sequence
+#             wtfreq = float(FREQS[(aalist.index(wtaa)),index])
+#             consensusfreq = float(FREQS[(aalist.index(consensus)),index])
+#             if (ratio * wtfreq) < consensusfreq: #if the consensus of a residue is greater than the threshold
+#                 #print "Residue number " + str(int(index) + 1)
+#                 #print str(int(100*consensusfreq)) + '% is at least ' + str(ratio) + ' times greater than ' + str(int(100*wtfreq)) + '%'
+#                 thissuggestion=res_to_change(wtaa,(index+1), consensus, consensusfreq, wtfreq)
+#                 mutations.append(thissuggestion)
+#     return mutations
 
 #Returns a list of suggested amiono acid mutations when given a query sequence, 
 #frequency array, and cutoff for the consensus threshold. Will suggest mutations to consensus when query amino 
 #acids that differ from the consensus and the consensus is at least cutoff.
 #If given a filename, suggested mutations will be saved as txt file.
-def cutoffconsensus(query, FREQS, cutoff):    
-    mutations=[]
-    aalist = IDS.flatten().tolist()
-    for index in range(len(FREQS[0,:])): #for each AA position
-        wtaa = query[index]
-        consensus = IDS[np.argmax(FREQS[:20,index]),0]
-        if wtaa != consensus: #cehck if the consens residue is different than the query sequence
-            wtfreq = float(FREQS[(aalist.index(wtaa)),index])
-            consensusfreq = float(FREQS[(aalist.index(consensus)),index])
-            if float(cutoff) < consensusfreq: #if the consensus of a residue is greater than the threshold
-                print "Residue number " + str(int(index) + 1)
-                print str(int(100*max(FREQS[:20,index]))) + "% is greater than or equal to " + str(int(100*float(cutoff))) + "%"
-                thissuggestion=res_to_change(wtaa, (index+1), consensus, consensusfreq, wtfreq)
-                mutations.append(thissuggestion)
-    return mutations
+# def cutoffconsensus(query, FREQS, cutoff):    
+#     mutations=[]
+#     aalist = IDS.flatten().tolist()
+#     for index in range(len(FREQS[0,:])): #for each AA position
+#         wtaa = query[index]
+#         consensus = IDS[np.argmax(FREQS[:20,index]),0]
+#         if wtaa != consensus: #cehck if the consens residue is different than the query sequence
+#             wtfreq = float(FREQS[(aalist.index(wtaa)),index])
+#             consensusfreq = float(FREQS[(aalist.index(consensus)),index])
+#             print str(int(100*max(FREQS[:20,index]))) + "% is greater than or equal to " + str(int(100*float(cutoff))) + "%"
+#             thissuggestion=res_to_change(wtaa, (index+1), consensus, consensusfreq, wtfreq)
+#             mutations.append(thissuggestion)
+#     return mutations
     
 #Takes a list of suggested mutations as 'res_to_change' objects, sorts by % conserved, removes duplicates.
 #Returns modified mutations array in with TYPE data types, and human readable suggested mutations list.
@@ -189,32 +191,25 @@ def formatmutations(mutations_with_dups,settings):
     SUGGESTED_MUTATIONS.append("These mutations may stabilize your protein since they differ from the consensus residue")
     if not len(mutations):
             SUGGESTED_MUTATIONS.append("No mutations found. Try reducing the ConsensusRatio or ConsensusThreshold in the config file. You could also try changing the BLAST parameters to adjust the number of sequences being returned (MaximumSequences and BlastEValue).")
-    else:
+    else: 
         for i in mutations: #for each suggested mutation
             SUGGESTED_MUTATIONS.append("Change " + i.wt + " " + str(i.res) + " to " + i.sug + " (" + str(int(100*i.freq)) + "% of similar proteins have " + i.sug +", only " + str(int(100*i.wtfreq)) + "% have "+ i.wt + ")" ) #add new suggestion on to any existing "SUGGESTED_MUTATIONS"
         #print mutations
         for i in mutations: #for each suggested mutation
-            CF.MACHLEARN_MUTATIONS.append(str(settings.RATIO)+', '+str(settings.MAXIMUMSEQUENCES)+', '+i.wt+', '+str(i.res)+', '+i.sug)
+            CF.MACHLEARN_MUTATIONS.append(str(settings.BLASTEVALUE)+', '+str(settings.MAXIMUMSEQUENCES)+', '+str(settings.USECOMPLETESEQUENCES)+', '+str(settings.MAXIMUMREDUNDANCYTHRESHOLD)+', '+str(settings.CONSENSUSTHRESHOLD)+', '+str(settings.RATIO)+', '+i.wt+', '+str(i.res)+', '+i.sug)
         #print "MACHINE LEARNING MUTATIONS ARRAY"
         #print CF.MACHLEARN_MUTATIONS
-    np.savetxt(("./completed/MACHLEARN_MUTATIONS_ARRAY.csv"),CF.MACHLEARN_MUTATIONS,delimiter=",",fmt="%s") #save file with AA names and counts
+    np.savetxt((HOME+"/completed/1BJP_MACHLEARN_MUTATIONS_ARRAY.csv"),CF.MACHLEARN_MUTATIONS,delimiter=",",fmt="%s") #save file with AA names and counts
 
     return mutations, SUGGESTED_MUTATIONS
 
 #define mutation list based on settings attributes of RATIO and/or CONSESUSTHRESHOLD and using trimmed alignment of sequences to identify query sequence (first sequence in alignment), and array of amino acid frequencies matching amino acid positions.
 def mutations(settings, alignment, freqs):
     print('\nIdentifying suggested mutations')
-    if settings.RATIO:
-        ratiomutations = ratioconsensus(alignment[0], freqs, settings.RATIO)
-    if settings.CONSENSUSTHRESHOLD:
-        thresholdmutations = cutoffconsensus(alignment[0], freqs, settings.CONSENSUSTHRESHOLD)
-        mutations = thresholdmutations
-        if settings.RATIO:
-            mutations = np.append(mutations, ratiomutations)
-    else:
-    	mutations = ratiomutations
-    mutations, output = formatmutations(mutations, settings)
-    return mutations, output
+    # thresholdmutations = cutoffconsensus(alignment[0], freqs, settings.CONSENSUSTHRESHOLD)
+    # mutations = thresholdmutations
+    # mutations, output = formatmutations(mutations, settings)
+    # return mutations, output
 #Save output suggestions file with any warnings that have been added
 def saveoutput(settings, warnings, output, filename):
     file = open(filename,'wb')
